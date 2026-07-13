8a6a935be55e5cf68c31da3695af0beaa945ac79
Yash Gajera <gajerayash2223@gmail.com>
2026-07-14 00:16:20 +0530

    devlopment

--- bill_system/settings.py
+++ bill_system/settings.py
@@ -5,23 +5,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-textile-billing-secret-key-change-in-prod'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "embrobill.pythonanywhere.com",
+]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'http://localhost:3001',
-    'http://127.0.0.1:3001',
-    'http://localhost:3002',
-    'http://127.0.0.1:3002',
-    'http://localhost:3003',
-    'http://127.0.0.1:3003',
-    'http://localhost:3004',
-    'http://127.0.0.1:3004',
-    'http://localhost:3005',
-    'http://127.0.0.1:3005',
+    "https://embrobill.pythonanywhere.com",
+
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 INSTALLED_APPS = [
@@ -80,9 +74,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = []
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
